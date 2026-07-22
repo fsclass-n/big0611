ef0a9c861047b6aba5e4e226c7bcaf0dbaff26fb
codelab <whyeil@naver.com>
2026-07-22 11:57:33 +0900

    11

---
+++ test20260722.py
@@ -0,0 +1,51 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "id": "5e1cf6d4-1d9b-4f2a-9070-207e29373913",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Hello Python\n"
+     ]
+    }
+   ],
+   "source": [
+    "print('Hello Python')"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "d4176e07-9f56-4bdf-9d7f-1c4eeba811d9",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python [conda env:.conda-myenv]",
+   "language": "python",
+   "name": "conda-env-.conda-myenv-py"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.11.15"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
