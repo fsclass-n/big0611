b65a92788206e8ce3bd810d7acb402cbf98c6997
fsclass-n <whyeil29@gmail.com>
2026-08-06 10:46:42 +0900

    11

---
+++ py/p03_크롤링/ch02_selenium/ex02_동적웹크롤링.py
@@ -0,0 +1,6 @@
+# 동적 웹 크롤링
+# selenium 사용 준비
+# 구글 크롬 브라우저: chrome://version/
+# 버전 151.0.7922.76(공식 빌드) (64비트)
+# https://sites.google.com/a/chromium.org/chromedriver/downloads -> 접속x
+# https://googlechromelabs.github.io/chrome-for-testing/
